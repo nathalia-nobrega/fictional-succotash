1ad3d7d8933f2e1ad587e016ee33a04899244b62
Nathalia Nóbrega <ttnast05@gmail.com>
2023-06-23 18:36:50 -0400

    refactoring

--- app/app/home.tsx
+++ app/app/home.tsx
@@ -3,10 +3,15 @@ import { ScrollView, View } from 'react-native'
 import { Props } from '.'
 import ListRecipe from '../src/components/ListRecipe'
 import Layout from './_layout'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function HomeScreen({ route, navigation }: Props) {
+  const { bottom, top } = useSafeAreaInsets()
   return (
-    <ScrollView className="flex">
+    <ScrollView
+      className="flex"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
       <View>
         {/* Start User profile and buttons */}
         <Layout route={route} navigation={navigation} />
